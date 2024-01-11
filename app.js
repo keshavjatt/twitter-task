7c957631dc45ac5c7c22aef6e9805b295683f07f
keshavjatt <thecoder780@gmail.com>
2024-01-11 11:45:30 +0530

    Automatic comment on twitter post

--- app.js
+++ app.js
@@ -2,9 +2,49 @@ const { Builder, By, until } = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 require('dotenv').config();
 
+async function loginTwitter(driver) {
+    try {
+        await driver.get('https://twitter.com/login');
+
+        const usernameInput = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div/div[5]/label/div/div[2]/div/input')), 10000);
+        await usernameInput.sendKeys(process.env.USER_NAME);
+
+        const nextButton = await driver.findElement(By.xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div/div[6]'));
+        await nextButton.click();
+
+        const passwordInput = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div[1]/div/div/div/div[3]/div/label/div/div[2]/div[1]/input')), 10000);
+        await passwordInput.sendKeys(process.env.PASSWORD);
+
+        const loginButton = await driver.findElement(By.xpath('//div[@data-testid="LoginForm_Login_Button"]'));
+        await loginButton.click();
+
+        await driver.wait(until.urlContains('twitter.com/home'), 10000);
+    } catch (e) {
+        console.error(e, "error detected");
+    }
+}
+
+async function postComment(driver, newTweet) {
+    try {
+        const replyButton = await driver.wait(until.elementLocated(By.xpath("//div[@data-testid='reply']")), 10000);
+        await replyButton.click();
+
+        const textArea = await driver.wait(until.elementLocated(By.xpath("//div[@data-testid='tweetTextarea_0']")), 10000);
+        await textArea.sendKeys(newTweet);
+
+        const replyPostButton = await driver.wait(until.elementLocated(By.xpath("//div[@data-testid='tweetButton']")), 10000);
+        await replyPostButton.click();
+
+        console.log('Comment posted successfully');
+    } catch (err) {
+        console.error(`POST COMMENT ERROR: ${err}`);
+    }
+}
+
 async function monitorTweets() {
     let options = new chrome.Options();
-    options.addArguments('--window-size=700,700');
+    options.windowSize({ width: 700, height: 812 });
+    // options.headless();
 
     let driver = await new Builder()
         .forBrowser('chrome')
@@ -14,8 +54,6 @@ async function monitorTweets() {
     try {
         await loginTwitter(driver);
 
-        await clickOnExistingTweets(driver);
-
         let lastTweet = await getLatestTweet(driver);
 
         while (true) {
@@ -24,20 +62,33 @@ async function monitorTweets() {
                 let newTweet = await getLatestTweet(driver);
 
                 if (newTweet !== lastTweet) {
-                    console.log(`NEW TWEET DETECTED: ${newTweet}`);
-                    await clickLatestTweet(driver);
-                    console.log("Clicked on the latest tweet.");
+                    console.log(`NEW TWEET DETECTED`);
                     lastTweet = newTweet;
-
-                    await driver.sleep(5000);
-                    await driver.get('https://twitter.com/home');
                 } else {
                     console.log(`NO NEW TWEET`);
                 }
+
+                await postComment(driver, newTweet);
+
+                let tweetElements = await driver.findElements(By.xpath("//*[@data-testid='tweetText']"));
+                for (let i = 0; i < tweetElements.length; i++) {
+                    try {
+                        tweetElements = await driver.findElements(By.xpath("//*[@data-testid='tweetText']"));
+                        if (tweetElements[i]) {
+                            await tweetElements[i].click();
+                            await driver.sleep(5000);
+                            await driver.navigate().back();
+                            await driver.sleep(5000);
+                        }
+                    } catch (err) {
+                        console.error(`ERROR CLICKING TWEET: ${err}`);
+                        tweetElements = await driver.findElements(By.xpath("//*[@data-testid='tweetText']"));
+                    }
+                }
             } catch (err) {
-                console.error(`ERROR: ${err}`);
+                console.error(`LOOP ERROR: ${err}`);
             }
-            await driver.sleep(20000);
+            await driver.sleep(5000);
         }
     } catch (err) {
         console.error(`MAIN ERROR: ${err}`);
@@ -46,38 +97,23 @@ async function monitorTweets() {
     }
 }
 
-async function loginTwitter(driver) {
-    await driver.get('https://twitter.com/login');
-    const usernameInput = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div/div[5]/label/div/div[2]/div/input')), 100000);
-    await usernameInput.sendKeys(process.env.USER_NAME);
-
-    const passwordInput = await driver.wait(until.elementLocated(By.xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[2]/div[2]/div[1]/div/div/div/div[3]/div/label/div/div[2]/div[1]/input')), 10000);
-    await passwordInput.sendKeys(process.env.PASSWORD);
-
-    const loginButton = await driver.findElement(By.xpath('//div[@data-testid="LoginForm_Login_Button"]'));
-    await loginButton.click();
-
-    await driver.wait(until.urlContains('twitter.com/home'), 10000);
-}
-
-async function clickOnExistingTweets(driver) {
-    const tweetElements = await driver.findElements(By.xpath("//div[@data-testid='tweetText']"));
-
-    for (const tweetElement of tweetElements) {
-        await tweetElement.click();
-        console.log("Clicked on an existing tweet.");
-        await driver.sleep(3000);
+async function getLatestTweet(driver) {
+    try {
+        const tweets = await driver.wait(until.elementsLocated(By.xpath("//*[@data-testid='tweetText']")), 60000);
+        const tweetsCaption = [];
+        for (let tweet of tweets) {
+            const text = await tweet.getText();
+            tweetsCaption.push(text);
+        }
+        const randomTweetCaption = tweetsCaption[Math.floor(Math.random() * tweetsCaption.length)];
+        console.log(`Randomly selected tweet: ${randomTweetCaption}`);
+        return randomTweetCaption;
+    } catch (err) {
+        console.error(`GET LATEST TWEET ERROR: ${err}`);
+        return null;
     }
 }
 
-async function getLatestTweet(driver) {
-    const tweetElement = await driver.wait(until.elementLocated(By.xpath("//div[@data-testid='tweetText']")), 20000);
-    return await tweetElement.getText();
-}
-
-async function clickLatestTweet(driver) {
-    const tweetElement = await driver.wait(until.elementLocated(By.xpath("//div[@data-testid='tweetText']")), 20000);
-    await tweetElement.click();
-}
-
-monitorTweets();
+(async () => {
+    await monitorTweets();
+})();
